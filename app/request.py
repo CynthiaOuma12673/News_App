6d51504301a27cc12608ba120cc85965b92602c7
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-01 01:41:35 +0300

    it will set all our properties and assign them the value none

--- app/request.py
+++ app/request.py
@@ -1,3 +1,9 @@
 import urllib.request, json
 from flask.templating import render_template
 from .models import Article,Source
+
+
+api_key = None
+category_url = None
+source_url = None
+headline_url = None
