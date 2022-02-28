c94601eb4aeff9067b57ed2ba05d5ae215948c0d
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-28 12:33:03 +0300

    it will import the urllib and models and then get the base url

--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+import urllib.request,json
+from .models import new
+
+New = new.New
 
 #Getting api key
 api_key = app.config[NEWS_API_KEY]
+
+#getting api url
+base_url = app.config['NEWS_API_BASE_URL']
