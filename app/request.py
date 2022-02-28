55f2c12a5cad48da09f2efe9eef166acfbc44ba2
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-28 12:42:10 +0300

    it will craete a function that will help get news details from the url

--- app/request.py
+++ app/request.py
@@ -9,3 +9,21 @@ api_key = app.config[NEWS_API_KEY]
 
 #getting api url
 base_url = app.config['NEWS_API_BASE_URL']
+
+def get_news(category):
+    '''
+    a function that gets the json response to our urls request
+    '''
+
+    get news_url = base_url.format(category, api_key)
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data =url.read()
+        get_news_response = json.loads(get_news_data)
+
+        new_results = None 
+
+        if get_news_response['results']:
+            new_results_list = get_news_response['results']
+            new_results = process_results(new_results_list)
+
+    return new_results
