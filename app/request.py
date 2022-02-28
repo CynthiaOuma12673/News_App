1b23524b57de9decedcd4e3f6e9b06ac889874d2
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-01 01:48:56 +0300

    it will create a function that will configure the requests

--- app/request.py
+++ app/request.py
@@ -7,3 +7,18 @@ api_key = None
 category_url = None
 source_url = None
 headline_url = None
+
+def configure_request(app):
+    '''
+    this is a function that will configure the requests
+    '''
+    global 
+    api_key, 
+    source_url, 
+    category_url, 
+    headline_url
+
+    api_key = app.config['NEWS_API_KEY']
+    category_url = app.config['CATEGORY_URL']
+    source_url = app.config['SOURCE_URL']
+    headline_url = app.config['HEADLINE_URL']
