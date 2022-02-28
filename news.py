6d96255ad7670d1f26df0730389aef147c0cce8b
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-28 10:55:40 +0300

    it will create other files and create a virtual environment

--- news.py
+++ news.py
@@ -13,14 +13,12 @@ def Index():
     articles = top_headlines['articles']
 
     description = []
-    # news = []
     image = []
     time = []
 
     for i in range(len(articles)):
         all_articles = articles[i]  
 
-        # news.append(all_articles['title'])
         description.append(all_articles['description'])
         image.append(all_articles['urlToImage'])
         time.append(all_articles['publishedAt'])
@@ -29,6 +27,31 @@ def Index():
     articles_list = zip(description, image, time)
 
     return render_template('index.html', context = articles_list)
+    
+    # bbc news
+    @app.route('/bbc')
+    def bbc():
+        newsapi = NewsApiClient(api_key='4c3735dffe684922bb8b27745a5a30d1')
+        top_headlines = newsapi.get_top_headlines(sources = "bbc-news")
+
+
+        articles = top_headlines['articles']
+
+        description = []
+        image = []
+        time = []
+
+        for i in range(len(articles)):
+            all_articles = articles[i]  
+
+            description.append(all_articles['description'])
+            image.append(all_articles['urlToImage'])
+            time.append(all_articles['publishedAt'])
+
+
+        articles_list = zip(description, image, time)
+
+        return render_template('bbc.html', context = articles_list)
 
 
 if __name__ == "__main__":
