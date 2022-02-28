d2cd97d7c8e52055106ab6ca99ef9b1668b4810d
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-28 12:15:39 +0300

    it will define a new class and create and init method that will pass in the three parameters we need

---
+++ app/models/new.py
@@ -0,0 +1,8 @@
+class New:
+    '''
+    New class to define new objects
+    '''
+    def __init__(self,description, publishedAt, urlToImage):
+        self.description = description
+        self.publishedAt = publishedAt
+        self.urlToImage = urlToImage
--- app/models/new_test.py
+++ app/models/new_test.py
@@ -6,4 +6,16 @@ class NewTest(unittest.TestCase):
     '''
     Test class to test the behaviour of the New class
     '''
-    
+
+    def setUp(self):
+        '''
+        Set up method that will run before every test
+        '''
+        self.new_new = New()
+
+    def test_instance(self):
+        self.assertTrue(is instance(self.new_new, New))
+
+
+if __name__ == '__main__':
+    unittest.main()
