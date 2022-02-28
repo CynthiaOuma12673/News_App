ebaedf31bd7ca0b8aa16505a7ef4e0418d18f7ac
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-28 15:17:33 +0300

    it will add a function in the init file that will help in configurations of the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,28 @@
 from flask import Flask
-from .config import DevConfig
+from .config import config_options
 from flask_bootstrap import Bootstrap
 
+bootstrap =Bootstrap(app)
+
 # initializing application
-app = Flask(__name__,instance_relative_config = True)
+def create_application(config_name):
+    app = Flask(__name__)
 
-# setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
 
-bootstrap = Bootstrap(app)
 
-from app import views
+    # setting up configuration
+    app.config.from_object(config_options[config_name])
+
+    # initializing bootstrap
+    bootstrap.init_app(app)
+
+    # blueprint registration
+    from .main import main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .request import configure_request
+    configure_request(app)
+
+    return app
+
+
