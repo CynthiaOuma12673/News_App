88845ddf48fbba87ea143e7183ef3bb05b80b7c9
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-28 15:35:48 +0300

    it will create the source class that will define all the objects in the source

---
+++ app/main/source.py
@@ -0,0 +1,10 @@
+class Source:
+    '''
+    this is a class that defines the source objects
+    '''
+
+    def __init__(self,id,name,url,description):
+        self.id = id 
+        self.name = name
+        self.url = url
+        self.description = description
