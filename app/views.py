ea7b69fbe9ebe566f93c04b0bb665a5b3244f861
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-28 12:59:47 +0300

    it will display our popular news to the index tempalate

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 @app.route('/')
 def index():
@@ -7,5 +8,6 @@ def index():
     '''
     view root page that returns the index page and its data
     '''
+    pupular_news = get_news('popular')
     title = 'Home - Welcome to the News App'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, popular = popular_news)
