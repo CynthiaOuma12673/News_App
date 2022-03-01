3514a44d71409d3f8f7bc199bcc064c11899aafb
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-01 15:40:21 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -1,18 +1,18 @@
 from app import create_app
-from flask_script import Manager, Server
+# from flask_script import Manager, Server
 
 app = create_app('development')
 
-manager = Manager(app)
-manager.add_command('server', Server)
-@manager.command
-def test():
-    '''
-    This function will run the unit tests
-    '''
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner().run(tests)
+# manager = Manager(app)
+# manager.add_command('server', Server)
+# @manager.command
+# def test():
+#     '''
+#     This function will run the unit tests
+#     '''
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner().run(tests)
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
