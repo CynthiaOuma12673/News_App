f0c4a965ee0244036f1bd5a1efe92bc95c8eb80a
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-01 12:01:16 +0300

    last commit

---
+++ tests/articles_test.py
@@ -0,0 +1,20 @@
+import unittest
+from app.models import Article
+
+class ArticleTest(unittest.TestCase):
+    '''
+    Test class to test the behaviour of the Article class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every test
+        '''
+        self.new_article = Article('','','','')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article))
+
+
+if __name__ == '__main__':
+    unittest.main()
